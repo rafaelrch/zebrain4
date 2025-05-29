b0ecd1eab53f8d3034f01e1f7c33d4100fd3f94a
Rafael Almeida Rocha <rafaelrocha@MacBook-Air-de-Rafael.local>
2025-05-29 09:47:39 -0300

    primeiro commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Não use experimental.turbo para forçar o uso do Webpack
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
